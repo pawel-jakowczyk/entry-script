79c7e11fd38e8a29a4d96b5c68bbbc83a13c0e17
Paweł Jakowczyk <pawel.jakowczyk@getresponse.com>
2021-06-01 22:05:22 +0200

    add script and composer

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$autoloadPath = getenv('autoloadPath');
+$configPath = getenv('configPath');
+$debug = getenv('debug');
+
+require_once $autoloadPath;
+
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use PJ\Routing\RoutingMiddleware;
+use PJ\Middleware\HandlersFactoryRequestHandler;
+
+$debug ? Debug::enable() : ErrorHandler::register();
+
+$routingMiddleware = RoutingMiddleware::create(require $configPath);
+$response = $routingMiddleware->process(
+    ServerRequestFactory::fromGlobals(),
+    new HandlersFactoryRequestHandler(),
+);
+(new SapiEmitter())->emit($response);

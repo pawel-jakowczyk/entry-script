e9ce2663f5a8e3e87a79037822e80c59353dac40
Paweł Jakowczyk <pawel.jakowczyk@getresponse.com>
2021-06-02 09:27:29 +0200

    index script refactor

--- index.php
+++ index.php
@@ -7,16 +7,16 @@ require_once $autoloadPath;
 
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\ErrorHandler\ErrorHandler;
 use PJ\Routing\RoutingMiddleware;
 use PJ\Middleware\MiddlewareFactoryRequestHandler;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 $debug ? Debug::enable() : ErrorHandler::register();
 
-$routingMiddleware = RoutingMiddleware::create(require $configPath);
-$response = $routingMiddleware->process(
-    ServerRequestFactory::fromGlobals(),
-    new MiddlewareFactoryRequestHandler(),
+(new SapiEmitter())->emit(
+    RoutingMiddleware::create(require $configPath)->process(
+        ServerRequestFactory::fromGlobals(),
+        new MiddlewareFactoryRequestHandler(),
+    )
 );
-(new SapiEmitter())->emit($response);
